7869f7f1f88bdf1a16daf0b2942d96b1deee8b27
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-21 18:14:59 +0530

    c

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 import path from "path";
-import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteCommonjs(), react()],
+  plugins: [react()],
   resolve: {
     alias: [
       {
@@ -14,17 +12,5 @@ export default defineConfig({
         replacement: path.resolve(__dirname, "src"),
       },
     ],
-  },
-  build: {
-    outDir: "build",
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        legacy({
-          targets: ["defaults", "not IE 11"],
-        }),
-      ],
-    },
   },
 });
