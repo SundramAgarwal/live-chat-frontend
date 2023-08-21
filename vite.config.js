400a91c47ed728f23263f222dab15ac57eb4647c
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-21 15:33:43 +0530

    commit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [viteCommonjs(), react()],
   resolve: {
     alias: [
       {
@@ -12,5 +13,13 @@ export default defineConfig({
         replacement: path.resolve(__dirname, "src"),
       },
     ],
+  },
+  build: {
+    outDir: "build",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [esbuildCommonjs(["react-s3"])],
+    },
   },
 });
